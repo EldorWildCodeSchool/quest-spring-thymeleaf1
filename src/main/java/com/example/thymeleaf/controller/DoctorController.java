a410e5480daacf812c429bd2c520b03c41142542
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-11-03 14:56:45 +0100

    Kackdreck

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,13 +1,19 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    public String doctor() {
+    @GetMapping("/doctor")
+    public String doctor(Model model, @RequestParam(value = "name", required = true, defaultValue = "John Smith") String name,
+                         @RequestParam(value = "number", required = true, defaultValue = "0") String number) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("number", number);
 
         return "doctor";
     }
--- src/main/java/com/example/thymeleaf/controller/IndexController.java
+++ src/main/java/com/example/thymeleaf/controller/IndexController.java
@@ -6,6 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private String name;
+    private String number;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+
     @GetMapping("/")
     public String home() {
         return "index";
