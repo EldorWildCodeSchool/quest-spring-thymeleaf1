367f9b3e14d3e83c036b23a40ebff067125da697
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-11-04 07:07:15 +0100

    <h1 th:text="Irgendwie hingefummelt. Update des action-attribute im form-tag in index.html und Anpassung des IndexController für die Basis-URL ("/")>Kackdreck</h1>

--- src/main/java/com/example/thymeleaf/controller/IndexController.java
+++ src/main/java/com/example/thymeleaf/controller/IndexController.java
@@ -1,35 +1,20 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     private String name;
-    private String number;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-
+    private int number;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("action", "doctor");
         return "index";
+
     }
 }
